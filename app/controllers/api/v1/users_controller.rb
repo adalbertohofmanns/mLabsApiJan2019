3fe96409e86d174835be69411ed513982f2cdc19
Adalberto Hofmann <adalberto@css4html.com.br>
2019-02-13 11:15:36 -0200

    criando user via api

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::UsersController < ApplicationController
+
+  respond_to :json
+  # POST /api/v1/users
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # Params Permitted.
+  def user_params
+    params.require(:user).permit(:name, :cpf, :email, :password)
+  end
+
+end
